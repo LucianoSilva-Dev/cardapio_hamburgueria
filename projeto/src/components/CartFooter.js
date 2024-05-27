8b083d0dbac6341bd08d16b2de50258778de10c0
LucianoDev <lucianonascimento2626@gmail.com>
2024-05-27 20:16:34 -0300

    Pequenas mudanças no Componente

--- projeto/src/components/CartFooter.js
+++ projeto/src/components/CartFooter.js
@@ -1,11 +1,10 @@
 import {FaShoppingCart} from 'react-icons/fa'
 import Styles from './CartFooter.module.css';
 
-function CartFooter(){
+function CartFooter({clickHandler}){
     return (
         <div className={Styles.box}>
-            <p className={Styles.text}>(1) veja seu carrinho </p>
-            <FaShoppingCart size={20} color='white'/>
+            <button onClick={() => clickHandler(true)} className={Styles.text}>(1) veja seu carrinho <FaShoppingCart size={20} color='white'/></button>
         </div>
     )
 }
