4d6f7eff658b2504c8d17be6f550acbf2ce4d867
LucianoDev <lucianonascimento2626@gmail.com>
2024-05-27 20:17:54 -0300

    Adicionado lógica para abrir e fechar o modal do carrinho de itens

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -2,14 +2,19 @@ import './App.css';
 
 import Header from './components/Header';
 import Menu from './components/Menu';
-import CartFooter
- from './components/CartFooter';
+import CartFooter from './components/CartFooter';
+import CartModal from './components/CartModal';
+import { useState } from 'react';
+
 function App() {
+  let [modal, SetModal] = useState(false);
+
   return (
     <div className="App">
       <Header />
       <Menu />
-      <CartFooter />
+      <CartFooter clickHandler={SetModal}/>
+      { modal && <CartModal fecharHandler={SetModal}/>}
     </div>
   );
 }
