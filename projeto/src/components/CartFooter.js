73e40b366676a75084e0fb2085b133e783c64241
LucianoDev <lucianonascimento2626@gmail.com>
2024-05-28 21:31:15 -0300

    Retirado '(1) ' do texto do button

--- projeto/src/components/CartFooter.js
+++ projeto/src/components/CartFooter.js
@@ -4,7 +4,7 @@ import Styles from './CartFooter.module.css';
 function CartFooter({clickHandler}){
     return (
         <div className={Styles.box}>
-            <button onClick={() => clickHandler(true)} className={Styles.text}>(1) veja seu carrinho <FaShoppingCart size={20} color='white'/></button>
+            <button onClick={() => clickHandler(true)} className={Styles.text}>veja seu carrinho <FaShoppingCart size={20} color='white'/></button>
         </div>
     )
 }
