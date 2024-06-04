2f62870513ecfef5e17b08c6152949b21e00cff5
LucianoDev <lucianonascimento2626@gmail.com>
2024-06-04 20:48:45 -0300

    Adicionado lógica para verificar se o restaurante está aberto/fechado e pequeno fix no css

--- projeto/src/components/Header.js
+++ projeto/src/components/Header.js
@@ -1,12 +1,28 @@
 import Styles from './Header.module.css'
-
+import React from 'react'
 function Header(){
+    //começa fechado
+    let [isOpen, SetIsOpen] = React.useState({backgroundColor: "#ff3131", color: "black"})
+
+    function VerifyRestaurantOpen(){
+        const hour = new Date().getHours()
+        if (hour >= 18 && hour < 22){
+            SetIsOpen({backgroundColor: "#54CC0A", color: "white"})
+        }
+    }
+
+    React.useEffect(() => {
+        VerifyRestaurantOpen(); // Chama a função para verificar o horário de funcionamento
+      }, []); // [] == executa a função apenas um vez no inicio do app
+              // nada == executa sempre que algo mudar
+              // [item1, item2] == executa sempre que um dos itens mudar
+
     return(
         <header className={Styles.header}>
             <img className={Styles.logo} src="./assets/hamb-1.png" alt="" />
             <h1 className={Styles.title}>Red Burguer</h1>
             <p className={Styles.end}>Rua dev sucesso, 12, Campo Grande, MS</p>
-            <div className={Styles.div}>Seg á Dom - 18:00 as 22:00</div>
+            <div style={isOpen} className={Styles.div}>Seg á Dom - 18:00 as 22:00</div>
         </header>
     )
 }
